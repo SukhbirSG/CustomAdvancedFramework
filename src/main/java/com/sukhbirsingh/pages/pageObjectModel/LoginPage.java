ddd3fd2992ff2ad5d7e25b716aad1aa62951d22e
HP <sukhveergill18@gmail.com>
2025-06-29 17:00:11 +0530

    Folder Structure Ready

---
+++ src/main/java/com/sukhbirsingh/pages/pageObjectModel/LoginPage.java
@@ -0,0 +1,4 @@
+package com.sukhbirsingh.pages.pageObjectModel;
+
+public class LoginPage {
+}
--- src/test/java/com/sukhbirsingh/SampletestNG/testclass.java
+++
@@ -1,7 +0,0 @@
-package com.sukhbirsingh.SampletestNG;
-
-public class testclass {
-    public static void main(String[] args) {
-        System.out.println("Why?");
-    }
-}
